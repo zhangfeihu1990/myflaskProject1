bb02592f40661c4552b42b69eaf293afff25aed9
sino_zhang <963076844@qq.com>
2017-04-13 17:33:16 +0800

    爬虫测试2

--- testscrape.py
+++ testscrape.py
@@ -22,31 +22,33 @@ def download(url,user_agent='wswp',numRetries=2,scrape_callback=None):
     soup = BeautifulSoup(html,"html.parser")
     allprice = soup.find_all('p',attrs={"class":"price"})
     for price in allprice:
+        #print price.text.split('\t')
+        regx = re.compile(r'[0-9]')
+        price = regx.findall(str(price))
         print price
 
-    # if scrape_callback:
-    #     links.extend(scrape_callback(html) or [])
-    sc = ScrapeCallback()
-    sc.__call__(html)
+    if scrape_callback:
+      links.extend(scrape_callback(html) or [])
     print 456
 
 
 class ScrapeCallback:
     def __init__(self):
-        #self.writer = csv.writer(open('f:/houses.csv','w'))
-        self.csv = open('f:/houses.csv','w')
+        self.writer = csv.writer(open('f:/houses.csv','w'))
+        #self.csv = open('f:/houses.csv','w')
         self.fields = ('price')
-        #self.writer.writerow(self.fields)
-        self.csv.write(self.fields)
+        self.writer.writerow(self.fields)
+        #self.csv.write(self.fields)
     def __call__(self,html):
       soup = BeautifulSoup(html,"html.parser")
       allprice = soup.find_all('p',attrs={"class":"price"})
       row = []
       for price in allprice:
-        #price = price.decode("gb2312")
-        self.csv.write(price)
-        #row.append(price)
-        #self.writer.writerow(row)
+
+        price1 = price.text.decode("utf-8")
+        #self.csv.write(price)
+        row.append(price)
+        self.writer.writerow(row)
 
 
 download("http://www.ziroom.com/z/nl/s4%E5%8F%B7%E7%BA%BF-t%E9%BB%84%E6%9D%91%E8%A5%BF%E5%A4%A7%E8%A1%97-z3.html"
